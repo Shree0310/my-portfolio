cd47a124e97a883264e52bb730283a74d5e6644c
sourashree banerjee <sourashree03@gmail.com>
2025-06-16 23:38:47 +0530

    Projects page im[rovements

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -20,12 +20,14 @@ const Projects = () => {
 
 
     useEffect(() => {
-        const timers = projects.map((project) => {
+        const timers = projects
+              .filter((proj) =>  proj.images && proj.images.length > 0)
+              .map((project) => {
             return setInterval(() => {
                 setActiveImage(prev => (
                     {
                         ...prev,
-                        [project.name]:(prev[project.name] + 1)% project.images.length
+                        [project.name]:(prev[project.name] + 1)% project.images!.length
                     }
                 ))
 
@@ -38,7 +40,7 @@ const Projects = () => {
         }
     },[])
     return <>
-         <div className="min-h-screen bg-[#1a1a2e] text-white">
+         <div className="min-h-screen bg-[#3e1f47] text-white">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <h1 className="text-4xl font-bold mb-8 text-center">My Projects</h1>
                 <div className="flex justify-center items-center gap-8">
@@ -47,9 +49,21 @@ const Projects = () => {
                         return (
                             <div key={project.name} className="relative rounded-md border-white">
                                <div className="">
-                               <img src={project.images[currentIndex]} alt="image"
-                                            className="w-[400px] h-[400px] rounded-md border-white p-3"/>
+                                {project.video ? (
+                                    <video src={project.video} 
+                                    autoPlay
+                                    loop
+                                    muted
+                                    playsInline
+                                    className="w-[400px] h-[200px] rounded-md border-white p-3 mt-5 mb-5"/>
+                                ) : (
+                                project.images && project.images.length > 0 && (
+                                    <img src={project.images[currentIndex]} alt="image"
+                                        className="w-[400px] h-[200px] rounded-md border-white p-3"/>
+                                    )
+                                )}
                                </div>
+                               <div className="w-[400px] py-1 mx-1 px-2 text-left text-sm">{project.description}</div>
                                 <div className="text-center my-2 text-xl font-semibold">{project.name}</div>
                                 <BorderBeam
                                 duration={4}
@@ -75,20 +89,27 @@ type Project = {
     name: string;
     description: string;
     url: string;
-    images: Array<string>; 
+    images?: Array<string>; 
+    video?: string
 }
 
 const projects: Project[]  = [
     {
         name: "Foodie",
-        description: "",
+        description: "An online food delivery app with features like showing your nearby food places, filtering different food types, offers going on and an option for you to add different dishes from restaurants to your cart and place orders.",
         url: "https://foodie.sourashreeart.com/",
         images: ["/Images/FoodieImage1.png", "/Images/FoodieImage2.png", "/Images/FoodieImage3.png"]
     },
     {
         name: "MoviesGpt",
-        description: "",
+        description: "A netflix clone to browse through different series and movies that you might wnat to watch, giving you an option to see the trailer and carousel of movies displayed with an ooption to ask AI to recommend you movies based on your mood and taste.",
         url: "https://moviesgpt.sourashreeart.com/#/",
         images: ["/Images/MoviesGPT1.png", "/Images/MoviesGPT2.png", "/Images/MoviesGPT3.png"]
+    },
+    {
+        name: "Achievr",
+        description: " A kanban board to track your personal and professional tasks, create new tasks and cycles.",
+        url: "https://achievr.sourashreeart.com/",
+        video: "/Videos/achievrDemo.mp4"
     }
 ]
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/Components/Header';
 import Projects from '@/Components/Projects';
 import React from 'react';
 
 const ProjectsPage = () => {
     return (
-        <div className="min-h-screen bg-[#1a1a2e] text-white">
+        <div className="min-h-screen bg-[#3e1f47] text-white">
+            <Header/>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="">
                     <Projects/>
