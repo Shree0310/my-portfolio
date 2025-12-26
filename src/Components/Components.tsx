a617551936b5f489ef6d6d63566543aea794ed7d
Sourashree <sourashree03@gmail.com>
2025-12-26 12:32:33 +0530

    added more button types

--- src/Components/Components.tsx
+++ src/Components/Components.tsx
@@ -1,24 +1,56 @@
 "use client"
 
-import ButtonUI, { BorderBeamButton, SimpleButton } from "./Playground/Button";
+import { cn } from "@/lib/utils";
+import ButtonUI, { BorderBeamButton, DisabledButton, LinkButton, SecondaryButton, SimpleButton } from "./Playground/Button";
 
 const Components = () => {
     return <div className="div">
         <h3 className="text-xl py-6 font-semibold dark:text-neutral-300 text-neutral-600">Components</h3>
-        <div className="grid grid-cols-3 gap-2 ">
-            <div className="dark:bg-neutral-800 h-64 w-64 rounded-md shadow-2xl border border-neutral-300 dark:border-neutral-400">
-                <h1 className="p-2 dark:text-neutral-300  text-neutral-600 text-[16px] border-b dark:border-neutral-300 border-neutral-600 font-semibold">Button</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+            <Card>                
+                <CardHeading><p>Button</p></CardHeading>
                 <ButtonUI/>
-            </div>
-             <div className="dark:bg-neutral-800 h-64 w-64 rounded-md shadow-2xl border border-neutral-300 dark:border-neutral-400">
-                <h1 className="p-2 dark:text-neutral-300  text-neutral-600 text-[16px] border-b dark:border-neutral-300 border-neutral-600 font-semibold">Border Beam Button</h1>
-                    <BorderBeamButton/>
-            </div>
-             <div className="dark:bg-neutral-800 h-64 w-64 rounded-md shadow-2xl border border-neutral-300 dark:border-neutral-400">
-                <h1 className="p-2 dark:text-neutral-300  text-neutral-600 text-[16px] border-b dark:border-neutral-300 border-neutral-600 font-semibold">Usual</h1>
+            </Card>
+            <Card>                
+                <CardHeading><p>Border Beam Button</p></CardHeading>
+                <BorderBeamButton/>
+            </Card>
+            <Card>                
+                <CardHeading><p>Usual</p></CardHeading>
                 <SimpleButton/>
-            </div>
+            </Card>
+            <Card>                
+                <CardHeading><p>Secondary Button</p></CardHeading>
+                <SecondaryButton/>
+            </Card>
+            <Card>                
+                <CardHeading><p>Disabled Button</p></CardHeading>
+                <DisabledButton/>
+            </Card>
+            <Card>
+                <CardHeading><p>Link Button</p></CardHeading>
+                <LinkButton/>
+            </Card>
         </div>
         </div>
 }
 export default Components;
+
+export const Card = ({
+        children, 
+        className
+    }: {
+        children?: React.ReactNode, 
+        className?: string
+    }) => {
+    return <div className={cn("dark:bg-neutral-800 h-72 w-72 rounded-md shadow-2xl border border-neutral-300 dark:border-neutral-600",className)}>
+        {children}
+    </div>
+}
+
+export const CardHeading = ({as: Tag = "p",children, className}:{as?: "p",children?: React.ReactElement, className?: string}) => {
+    return <Tag className={cn("p-2 dark:text-neutral-300  text-neutral-600 text-[16px] border-b dark:border-neutral-600 border-neutral-300 font-semibold", className)}>
+        {children}
+    </Tag>
+
+}
--- src/Components/Playground/Button.tsx
+++ src/Components/Playground/Button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import BorderBeam from "../ui/BorderBeam";
 
 const ButtonUI = ({children, className}:{children?:React.ReactElement, className?: string}) => {
-    return <div className={cn(" flex items-center justify-center h-64 w-64 pb-10 shadow-2xl", className)}>
+    return <div className={cn(" flex items-center justify-center h-64 w-64 pb-10 ", className)}>
         <Button className="bg-linear-to-bl from-blue-700 via-blue-600 to-blue-700 text-neutral-50 rounded-4xl shadow-2xl h-12 w-28">Click here</Button>
         {children}
     </div>
@@ -30,7 +30,28 @@ export const BorderBeamButton = () => {
 
 
 export const SimpleButton = () => {
-    return <div className={cn(" flex items-center justify-center h-64 w-64 pb-10 shadow-2xl")}>
-        <Button className="bg-neutral-200 text-neutral-500 rounded-sm shadow-2xl h-8 w-20">Click here</Button>
+    return <div className={cn(" flex items-center justify-center h-64 w-64 pb-10 ")}>
+        <Button className="bg-neutral-200 text-neutral-500 rounded-sm shadow-2xl h-10 w-24">Click here</Button>
+    </div>
+}
+
+export const SecondaryButton = () => {
+    return <div className={cn(" flex items-center justify-center h-64 w-64 pb-10 ")}>
+        <Button className="bg-neutral-700 text-neutral-50 rounded-sm shadow-2xl h-10 w-24">Click here</Button>
+    </div>
+}
+
+export const DisabledButton = () => {
+    return <div className={cn(" flex items-center justify-center h-64 w-64 pb-10 ")}>
+        <Button className="bg-neutral-700/70 opacity-60 text-neutral-50/70 pointer-events-none rounded-sm shadow-2xl h-10 w-24">Click here</Button>
+    </div>
+}
+
+export const LinkButton = () => {
+    return <div className={cn(" flex flex-col mx-auto items-center justify-center h-64 w-64 pb-10 ")}>
+        <Button className="font-semibold text-sky-500 hover:text-sky-300 border-none shadow-2xl ">Primary</Button>
+        <Button className="font-semibold text-orange-400 hover:text-orange-300 border-none shadow-2xl">Secondary</Button>
+        <Button className="font-semibold text-green-600 hover:text-green-400 border-none shadow-2xl">Success</Button>
+        <Button className="font-semibold text-red-500 hover:text-red-300 border-none shadow-2xl">Danger</Button>
     </div>
 }
