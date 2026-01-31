a6299de9dbe0ef79264d5e5025b7271562477014
Sourashree <sourashree03@gmail.com>
2026-01-31 23:46:58 +0530

    Added state for the icons in contexual AI bar

--- src/components/Playground/Bar/ContexualAIBar.tsx
+++ src/components/Playground/Bar/ContexualAIBar.tsx
@@ -4,24 +4,40 @@ import { IconScissors } from '@tabler/icons-react';
 import { IconClockHour5Filled } from '@tabler/icons-react';
 import { IconMicrophone2 } from '@tabler/icons-react';
 import { IconActivity } from '@tabler/icons-react';
+import { useState } from 'react';
+import { IconArrowRight } from '@tabler/icons-react';
 
 const ContexualAIBar = () => {
+
+    const [isMusicMode, setIsMusicMode] = useState(false);
+
+
     return <div className="flex flex-row justify-center items-center">
-        <div className="flex h-18 w-76 rounded-4xl dark:bg-neutral-800 bg-neutral-100 shadow-lg p-1">
+        <div className={`flex h-18 w-76 rounded-4xl dark:bg-neutral-800 bg-neutral-100 shadow-lg p-1
+                ${isMusicMode ===false ? "justify-between gap-8 p-1": ""}`}>
             <div className="flex w-28 h-full rounded-4xl dark:bg-neutral-900 bg-white shadow-sm p-2">
                 <div className="w-12 h-12 p-3 rounded-full dark:bg-neutral-800 bg-neutral-100">
-                    <IconPlaylist stroke={2} className='flex justify-center items-center w-7 h-7 text-black dark:text-neutral-400'/>
+                    <IconPlaylist stroke={2}
+                        onClick={() => setIsMusicMode(!isMusicMode)} 
+                        className='flex justify-center items-center w-7 h-7 text-black dark:text-neutral-400'/>
                 </div>
                 <div className="py-2 px-2">
                     <IconSparkles stroke={1.75} className='w-8 h-8 text-black dark:text-neutral-400' />
                 </div>
             </div>
-            <div className="flex gap-2 p-4">
-                <IconScissors stroke={2} className='w-8 h-8 text-black dark:text-neutral-400'/>
-                <IconClockHour5Filled className='w-8 h-8 text-black dark:text-neutral-400' />
-                <IconMicrophone2 stroke={2} className='w-8 h-8 text-black dark:text-neutral-400' />
-                <IconActivity stroke={2} className='w-8 h-8 text-black dark:text-neutral-400'/>
-            </div>
+            {isMusicMode ?
+                (<div className="flex gap-2 p-4">
+                    <IconScissors stroke={2} className='w-8 h-8 text-black dark:text-neutral-400'/>
+                    <IconClockHour5Filled className='w-8 h-8 text-black dark:text-neutral-400' />
+                    <IconMicrophone2 stroke={2} className='w-8 h-8 text-black dark:text-neutral-400' />
+                    <IconActivity stroke={2} className='w-8 h-8 text-black dark:text-neutral-400'/>
+                </div>)
+                : (
+                    <div className="w-14 h-14 p-3 m-1 flex justify-center rounded-full dark:bg-neutral-800 bg-white">
+                        <IconArrowRight stroke={2} className='w-8 h-8 text-black dark:text-neutral-400'/>
+                    </div>
+                )
+            }
             
         </div>
     </div>
