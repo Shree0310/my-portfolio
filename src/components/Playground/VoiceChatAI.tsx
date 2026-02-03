b9cfc504e084a50fbf4cf6a4d5c012620562fbb9
Sourashree <sourashree03@gmail.com>
2026-02-03 21:01:25 +0530

    Added necessary states for speech to text

--- src/components/Playground/VoiceChatAI.tsx
+++ src/components/Playground/VoiceChatAI.tsx
@@ -6,13 +6,19 @@ import { IconPlayerStopFilled } from '@tabler/icons-react';
 
 const VoiceChatAI = () => {
     const [isListening, setIsListening] = useState(false);
+    //Store the speech to text results
+    const [transcript, setTranscript] = useState('');
+    //Store the temporary results while speaking
+    const [interimTransript, setInterimTranscript] = useState('')
 
     return <div className="flex px-2 justify-center items-center">
         {isListening ?
             (<div className="flex justify-center gap-2 items-center flex-row h-64 w-64 border dark:border-neutral-500 shadow-md rounded-2xl bg-white dark:bg-neutral-800 p-2">
                 <div className="flex flex-col justify-center items-center gap-4">
                     <WaveFormAnimation isAnimating={isListening}/>
-                    <p className='text-neutral-500 dark:text-neutral-400'>Text generated from audio</p>
+                    <p className='text-neutral-500 dark:text-neutral-400'>
+                        {interimTransript || transcript || 'Listening...'}
+                    </p>
                     <div className="relative flex justify-center items-center w-12 h-12 rounded-full bg-indigo-300/40 dark:bg-indigo-400 cursor-pointer">
                         <div className='absolute w-5 h-5 rounded-full bg-indigo-600' ></div>
                     </div>
