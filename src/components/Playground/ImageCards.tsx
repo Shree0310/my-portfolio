4c84cd326b493625115a44d9945931cb10e16073
Sourashree <sourashree03@gmail.com>
2026-03-11 00:15:37 +0530

    added a new image cards in component

--- src/components/Playground/ImageCards.tsx
+++ src/components/Playground/ImageCards.tsx
@@ -1,35 +1,53 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import  peony  from "../../../public/Images/peony.jpg";
+import  leaves  from "../../../public/Images/leaves.jpg";
+import { useState } from 'react';
 
 
 const ImageCards = () => {
- return <div className='relative w-36 h-48'>
-    <motion.div 
-        className='w-48 h-56 rounded-2xl shadow-md absolute'
-        style={{ zIndex: 1}}>
-        <Image src={peony} alt="avatar" className="h-full w-full object-cover rounded-2xl"/>
-    </motion.div>
-    <motion.div 
-        className='w-48 h-56 rounded-2xl shadow-md absolute'
-        style={{
-            zIndex: 2,
-            rotate: -12,
-            transformOrigin: 'bottom center'
-        }}
-        initial={{
-            rotate: -12
-        }}
-        whileHover={{
-            rotate: -6
-        }}
-        transition={{
-            type: 'spring',
-            stiffness: 200,
-            damping:10
-        }}>
-        <Image src={peony} alt="avatar" className="h-full w-full object-cover rounded-2xl"/>
-    </motion.div>
+    const [expanded, setExpanded] = useState(false);
+
+ return <div className=''>
+    {expanded ? (<div className='flex flex-row gap-10'>
+            <motion.div 
+                initial={{}}
+                className='w-48 h-56 rounded-2xl shadow-md'>
+                <Image src={peony} alt="avatar" className="h-full w-full object-cover rounded-2xl"/>
+            </motion.div>
+            <div className='w-48 h-56 rounded-2xl shadow-md ' onClick={() => setExpanded(!expanded)}>
+                <Image src={leaves} alt="avatar" className="h-full w-full object-cover rounded-2xl"/>
+            </div>
+        </div>):
+     (<div className='relative w-36 h-48'>
+         <motion.div 
+            className='w-48 h-56 rounded-2xl shadow-md absolute'
+            style={{ zIndex: 1}}>
+            <Image src={peony} alt="avatar" className="h-full w-full object-cover rounded-2xl"/>
+        </motion.div>
+        <motion.div 
+            className='w-48 h-56 rounded-2xl shadow-md absolute'
+            style={{
+                zIndex: 2,
+                rotate: -12,
+                transformOrigin: 'bottom center'
+            }}
+            initial={{
+                rotate: -12
+            }}
+            whileHover={{
+                rotate: -6
+            }}
+            transition={{
+                type: 'spring',
+                stiffness: 200,
+                damping:10
+            }}
+            onClick={() => setExpanded(!expanded)}>
+            <Image src={leaves} alt="avatar" className="h-full w-full object-cover rounded-2xl"/>
+        </motion.div>
+    </div>
+    )}
  </div>
 }
 export default ImageCards;
