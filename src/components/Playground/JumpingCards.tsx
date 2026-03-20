a4b444d62477fa059891eb29fa8f33fd660fec5b
Sourashree <sourashree03@gmail.com>
2026-03-20 16:45:19 +0530

    Added layout Animations to the new expandable cards component

--- src/components/Playground/JumpingCards.tsx
+++ src/components/Playground/JumpingCards.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { cn } from '@/lib/utils';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 
 type Card = {
@@ -29,15 +29,23 @@ const JumpingCards = () => {
             }
             document.addEventListener('mousedown', handleOutsideClick);
         }
-        return () => document.removeEventListener('mousedown', handleOutsideClick)
+        return () => document.removeEventListener('mousedown', handleOutsideClick);
     },[]);
 
+    const isAnyCardActive = () => {
+        return active?.title;
+    }
+
+    const isCurrentActive  = (card: Card) => {
+        return active?.title === card.title;
+    }
+ 
     const cards = [
         {
             title: "Working Knowledge",
             description: "Frameworks, principles, and models I've learned and developed that you will be able to immediately apply to your practice.",
             skeleton: (
-                <div className='w-full h-30 bg-linear-to-r from-orange-700 to orange-700/50'></div>
+                <div className='w-full h-40 bg-linear-to-r from-orange-700 to orange-700/50'></div>
             ),
             className: 'bg-orange-600',
             config: {
@@ -51,7 +59,7 @@ const JumpingCards = () => {
             title: 'Practical Demostration',
             description: 'Detailed walkthroughs of designing interfaces, identifying opportunities, and improving through refinement.',
             skeleton: (
-                <div className='w-full h-30 bg-linear-to-r from-stone-500 to-stone-500/50'></div>
+                <div className='w-full h-40 bg-linear-to-r from-stone-500 to-stone-500/50'></div>
             ),
             className:'bg-stone-200 text-stone-600',
             config: {
@@ -65,7 +73,7 @@ const JumpingCards = () => {
             title:"Collaborating with AI",
             description: 'Video lessons on practical, specific methods of working with AI to get exacting results. Tools covered include Claude Code and v0.',
             skeleton: (
-                <div className='w-full h-30 bg-linear-to-r from-sky-700 to-sky-700/50'></div>
+                <div className='w-full h-40 bg-linear-to-r from-sky-700 to-sky-700/50'></div>
             ),
             className:'bg-sky-500',
             config: {
@@ -79,7 +87,7 @@ const JumpingCards = () => {
             title: 'Means & Methods',
             description: 'General tips and techniques to apply to your daily work to achieve excellence in interface design and assembly.',
             skeleton: (
-                <div className='w-full h-30 bg-linear-to-r from-emerald-600 to-emerald-600/50'></div>
+                <div className='w-full h-40 bg-linear-to-r from-emerald-600 to-emerald-600/50'></div>
             ),
             className: 'bg-emerald-400 text-stone-600',
             config: {
@@ -93,7 +101,7 @@ const JumpingCards = () => {
             title: 'Interface Kit',
             description: 'Screencasts, highlights, and deep dives that showcase the end-to-end journey for designing and building',
             skeleton: (
-                <div className='w-full h-30 bg-linear-to-r from-stone-400 to-stone-400/50'></div>
+                <div className='w-full h-40 bg-linear-to-r from-stone-400 to-stone-400/50'></div>
             ),
             className: 'bg-stone-700',
             config: {
@@ -104,7 +112,7 @@ const JumpingCards = () => {
             }
         }
     ]
-    return <div ref={ref} className="max-w-5xl mx-auto w-full h-96 relative mt-16 ml-20">
+    return <div ref={ref} className="max-w-5xl mx-auto w-full h-96 relative ml-20">
         {cards.map((card,index) => (
             <motion.div
                 key={index}
@@ -121,12 +129,12 @@ const JumpingCards = () => {
                         filter: "blur(10px)"
                     }}
                     animate={{
-                        x: card.config.x,
-                        y: card.config.y,
-                        rotate: card.config.rotate,
-                        scale: 1,
-                        width: 260,
-                        height: 300,
+                        x: isCurrentActive(card) ? 320 : (isAnyCardActive() ? card.config.x * 0.6 + 210: card.config.x),
+                        y: isCurrentActive(card) ? 0 : (isAnyCardActive() ? 400 : card.config.y),
+                        rotate:  isCurrentActive(card) ? 0 : (isAnyCardActive() ? card.config.rotate * 0.2 : card.config.rotate * 0.2),
+                        scale: isCurrentActive(card) ? 1 : (isAnyCardActive() ? 0.7 : 1),
+                        width: isCurrentActive(card) ? 400 : 320,
+                        height: isCurrentActive(card) ? 460 :  380,
                         filter: "blur(0px)"
                     }} 
                     transition={{
@@ -135,11 +143,19 @@ const JumpingCards = () => {
                         damping: 50
                     }}
                     whileHover={{
-                        scale: 1.05,
+                        scale: isCurrentActive(card) ? 1 : (isAnyCardActive() ? 0.5 : 1.05),
                     }}
-                    className={cn('w-80 font-signika absolute p-8 cursor-pointer inset-0 flex rounded-lg  flex-col items-start justify-between overflow-hidden', card.className )}>
+                    className={cn('w-70 font-signika absolute p-8 cursor-pointer inset-0 flex rounded-lg  flex-col items-start justify-between overflow-hidden', card.className )}>
                     {card.skeleton}
-                    <div className='text-2xl'>{card.title}</div>
+                    <motion.div layoutId={card.title + "title"} className='text-2xl'>{card.title}</motion.div>
+                    <AnimatePresence>
+                    {isAnyCardActive() && isCurrentActive(card) &&
+                        <motion.p 
+                            layoutId={card.title + "description"}
+                            className={cn('text-lg text-left', active?.className)}>
+                            {active?.description}
+                        </motion.p>}
+                    </AnimatePresence>
                 </motion.button>
             </motion.div>
         ))}
